fb431816d35dcfdab69d018d9ce9e9d7b510cfbc
Zongminxiekl <Zongminkl@gmail.com>
2020-09-05 22:45:53 +0800

    Qingmur first commit

---
+++ src/plugin/ElementUI.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import {
+  Row,
+  Col,
+  Input,
+  Button,
+  Form,
+  FormItem
+} from 'element-ui'
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Input)
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
